0b0ade8238a0d1ee31335654c4c8e9be437ccbb8
Moshe <moshe@zippo.io>
2020-09-03 01:46:50 +0300

    track allocPoint of pools

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,70 +1,41 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt } from "@graphprotocol/graph-ts";
 import {
-  Contract,
+  MasterChef,
   Deposit,
   EmergencyWithdraw,
   OwnershipTransferred,
-  Withdraw
-} from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+  Withdraw,
+  AddCall,
+  SetCall,
+} from "../generated/MasterChef/MasterChef";
+import { MasterChefPool } from "../generated/schema";
 
-export function handleDeposit(event: Deposit): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.user = event.params.user
-  entity.pid = event.params.pid
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.BONUS_MULTIPLIER(...)
-  // - contract.bonusEndBlock(...)
-  // - contract.devaddr(...)
-  // - contract.getMultiplier(...)
-  // - contract.migrator(...)
-  // - contract.owner(...)
-  // - contract.pendingSushi(...)
-  // - contract.poolInfo(...)
-  // - contract.poolLength(...)
-  // - contract.startBlock(...)
-  // - contract.sushi(...)
-  // - contract.sushiPerBlock(...)
-  // - contract.totalAllocPoint(...)
-  // - contract.userInfo(...)
-}
+export function handleDeposit(event: Deposit): void {}
 
 export function handleEmergencyWithdraw(event: EmergencyWithdraw): void {}
 
 export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
 
 export function handleWithdraw(event: Withdraw): void {}
+
+export function handleAddPool(event: AddCall): void {
+  let masterChef = MasterChef.bind(event.to);
+
+  let poolId = masterChef.poolLength().minus(BigInt.fromI32(1));
+  let poolInfo = masterChef.poolInfo(poolId);
+
+  let pool = new MasterChefPool(poolId.toString());
+  pool.balance = BigInt.fromI32(0);
+  pool.lpToken = poolInfo.value0;
+  pool.allocPoint = poolInfo.value1;
+  pool.lastRewardBlock = poolInfo.value2;
+  pool.accSushiPerShare = poolInfo.value3;
+
+  pool.save();
+}
+
+export function handleSetPoolAllocPoint(event: SetCall): void {
+  let pool = MasterChefPool.load(event.inputs._pid.toString());
+  pool.allocPoint = event.inputs._allocPoint;
+  pool.save();
+}
