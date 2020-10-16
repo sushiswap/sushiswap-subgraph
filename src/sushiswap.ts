1a45dfb81cdb6dbdef9adf7cd33c6399d9b9709a
jiro-ono <jirosushi@protonmail.com>
2020-10-16 01:06:57 -0400

    Fully implemented working version of subgraph with MasterChefPools current block support

---
+++ src/sushiswap.ts
@@ -0,0 +1,102 @@
+import { BigInt, Address, ethereum, log } from "@graphprotocol/graph-ts"
+import {
+  MasterChef,
+  Deposit,
+  EmergencyWithdraw,
+  OwnershipTransferred,
+  Withdraw,
+  SetCall,
+  MigrateCall
+} from "../generated/MasterChef/MasterChef"
+
+import {
+  MasterChef as MasterChefEntity,
+  MasterChefPool,
+} from "../generated/schema";
+
+const CHEF_ADDY = '0xc2edad668740f1aa35e4d8f227fb8e17dca888cd'
+
+export function handleDeposit(event: Deposit): void {
+  let pool = getPoolEntity(event.params.pid, event.block);
+  pool.balance = pool.balance + event.params.amount;
+  pool.save();
+}
+
+export function handleEmergencyWithdraw(event: EmergencyWithdraw): void {
+  let pool = getPoolEntity(event.params.pid, event.block);
+  pool.balance = pool.balance - event.params.amount;
+  pool.save();
+}
+
+export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+
+export function handleWithdraw(event: Withdraw): void {
+  let pool = getPoolEntity(event.params.pid, event.block);
+  pool.balance = pool.balance - event.params.amount;
+  pool.save();
+}
+
+export function handleSetPoolAllocPoint(event: SetCall): void {
+  let pool = getPoolEntity(event.inputs._pid, event.block);
+
+  // Update MasterChefEntity
+  let masterChefEntity = getMasterChefEntity()
+  masterChefEntity.totalAllocPoint = masterChefEntity.totalAllocPoint.plus(
+    event.inputs._allocPoint.minus(pool.allocPoint)
+  )
+  masterChefEntity.save();
+
+  // Update pool
+  pool.allocPoint = event.inputs._allocPoint;
+  pool.save();
+}
+
+export function handleMigrate(event: MigrateCall): void {
+  let masterChef = MasterChef.bind(event.to);
+
+  let pool = MasterChefPool.load(event.inputs._pid.toString());
+  pool.lpToken = masterChef.poolInfo(event.inputs._pid).value0;
+  pool.save();
+}
+
+function getPoolEntity(poolId: BigInt, block: ethereum.Block): MasterChefPool {
+  let masterChef = MasterChef.bind(Address.fromString(CHEF_ADDY));
+  let pool = MasterChefPool.load(poolId.toString());
+  let poolInfo = masterChef.poolInfo(poolId);
+
+  if (pool == null) {
+    // init new pool entity
+    pool = new MasterChefPool(poolId.toString());
+    pool.balance = BigInt.fromI32(0);
+    pool.addedBlock = block.number;
+    pool.addedTs = block.timestamp;
+
+    //update MasterChefEntity poolLength
+    let masterChefEntity = getMasterChefEntity();
+    masterChefEntity.poolLength = masterChef.poolLength();
+    masterChefEntity.save()
+  }
+
+  // Update pool
+  pool.lpToken = poolInfo.value0;
+  pool.allocPoint = poolInfo.value1;
+  pool.lastRewardBlock = poolInfo.value2;
+  pool.accSushiPerShare = poolInfo.value3;
+
+  pool.save()
+
+  return pool as MasterChefPool;
+}
+
+function getMasterChefEntity(): MasterChefEntity {
+  let entity = MasterChefEntity.load("1");
+
+  if (entity == null) {
+    entity = new MasterChefEntity("1");
+    entity.totalAllocPoint = BigInt.fromI32(0);
+    entity.poolLength = BigInt.fromI32(0);
+    entity.save();
+  }
+
+  return entity as MasterChefEntity;
+}
