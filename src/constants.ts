c87ed2cdd20446ccf5ce449b841aedfebe405139
Matthew Lilley <hello@matthewlilley.com>
2020-11-05 23:57:18 +0000

    Extract Timelock
    Timelock is lightweight, and takes an incredibly short amount of time to sync. It makes a good candidate for extraction.

--- src/constants.ts
+++ src/constants.ts
@@ -8,11 +8,13 @@ export const ZERO_BIG_INT = BigInt.fromI32(0)
 
 export const ONE_BIG_INT = BigInt.fromI32(1)
 
-export const NUMBER_LITERAL_1E18 = BigDecimal.fromString('1e18')
+export const BIG_DECIMAL_1E18 = BigDecimal.fromString('1e18')
+
+export const BIG_DECIMAL_1E12 = BigDecimal.fromString('1e12')
 
 export const MASTER_CHEF_ADDRESS = Address.fromString('0xc2edad668740f1aa35e4d8f227fb8e17dca888cd')
 
-export const SUSHIBAR_ADDRESS = Address.fromString('0x8798249c2e607446efb7ad49ec89dd1865ff4272')
+export const SUSHI_BAR_ADDRESS = Address.fromString('0x8798249c2e607446efb7ad49ec89dd1865ff4272')
 
 export const SUSHI_TOKEN_ADDRESS = Address.fromString('0x6b3595068778dd592e39a122f4f5a5cf09c90fe2')
 
