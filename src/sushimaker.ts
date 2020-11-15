53a03e165a720ba2915d545d1a2b3abb4e9eefbe
jiro-ono <jirosushi@protonmail.com>
2020-11-15 11:25:30 -0500

    Added pastServings to the bar subgraph

--- src/sushimaker.ts
+++ src/sushimaker.ts
@@ -6,9 +6,10 @@ import {
   BIG_INT_ZERO,
   SUSHI_TOKEN_ADDRESS,
   FACTORY_ADDRESS,
-  SUSHIMAKER_ADDRESS
+  SUSHIMAKER_ADDRESS,
+  SUSHIBAR_ADDRESS
 } from './constants'
-import { Address, BigDecimal, BigInt, dataSource, ethereum, log } from '@graphprotocol/graph-ts'
+import { Address, BigDecimal, BigInt, ByteArray, dataSource, ethereum, log } from '@graphprotocol/graph-ts'
 import { PendingServing, PastServing } from '../generated/schema'
 import { Pair as PairContract, Swap as SwapEvent } from '../generated/SushiMaker/Pair'
 import { Maker as MakerContract } from '../generated/SushiMaker/Maker'
@@ -52,5 +53,39 @@ export function handleBlock(block: ethereum.Block): void {
 }
 
 export function handleServeItUp(event: SwapEvent): void {
+  // check if it's a swap from SushiMaker to SushiBar
+  if (event.params.sender != SUSHIMAKER_ADDRESS && event.params.to != SUSHIBAR_ADDRESS) {
+    return
+  }
 
+  let convert_tx = event.transaction
+
+  let input_data = convert_tx.input.toHexString()
+  let token0 = Address.fromString(ByteArray.fromHexString(input_data.substring(34, 74)).toHexString())
+  let token1 = Address.fromString(ByteArray.fromHexString(input_data.substring(98, 138)).toHexString())
+
+  let factory = FactoryContract.bind(FACTORY_ADDRESS)
+  let lpToken = factory.getPair(token0, token1)
+  let pair = PairContract.bind(lpToken)
+
+  // build up new PastServing entity
+  let servingName = lpToken.toHexString() + '-' + event.block.number.toString()
+  let serving = new PastServing(servingName)
+  serving.from = convert_tx.from
+  serving.tx = convert_tx.hash
+  serving.lpToken = lpToken
+  serving.token0 = token0
+  serving.token1 = token1
+  serving.block = event.block.number
+  serving.blockTs = event.block.timestamp
+  serving.sushiAmount = event.params.amount0Out
+
+  serving.save()
+
+  // reset pendingServing
+  let pendingServing = PendingServing.load(lpToken.toHexString())
+  pendingServing.slpAmount = BIG_INT_ZERO
+  pendingServing.token0Amount = BIG_INT_ZERO
+  pendingServing.token1Amount = BIG_INT_ZERO
+  pendingServing.save()
 }
