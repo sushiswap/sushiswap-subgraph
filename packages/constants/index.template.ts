bc495cf8371a1126a6c8cde2859c3bccebffc280
0xJiro <jirosushi@protonmail.com>
2021-06-01 00:14:16 -0700

    Quick fixes

--- packages/constants/index.template.ts
+++ packages/constants/index.template.ts
@@ -30,7 +30,7 @@ export const LOCKUP_BLOCK_NUMBER = BigInt.fromI32(10959148)
 
 export const MASTER_CHEF_ADDRESS = Address.fromString('{{ masterchef_address }}')
 
-export const MASTER_CHEF_V2_ADDRESS = Address.fromString('0xEF0881eC094552b2e128Cf945EF17a6752B4Ec5d')
+export const MASTER_CHEF_V2_ADDRESS = Address.fromString('0xef0881ec094552b2e128cf945ef17a6752b4ec5d')
 
 export const SUSHI_BAR_ADDRESS = Address.fromString('{{ sushi_bar_address }}')
 
