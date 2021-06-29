d0dffb394b7ba9ed7deca42dc83cd6a7b5a3327f
Goose Gordon <gooseophocles@users.noreply.github.com>
2021-06-29 15:29:33 -0500

    rebuild complete

--- subgraphs/maker/src/maker.ts
+++ subgraphs/maker/src/maker.ts
@@ -1,10 +1,9 @@
-import { Address, ByteArray, log } from '@graphprotocol/graph-ts'
-import { BIG_DECIMAL_1E18, BIG_DECIMAL_ZERO, FACTORY_ADDRESS, SUSHI_BAR_ADDRESS, SUSHI_MAKER_ADDRESS } from 'const'
+import { Address, log } from '@graphprotocol/graph-ts'
+import { BIG_DECIMAL_ZERO, FACTORY_ADDRESS, SUSHI_MAKER_ADDRESS } from 'const'
 import { Maker, Server, Serving } from '../generated/schema'
-
-import { ERC20 as ERC20Contract } from '../generated/Maker/ERC20'
 import { Factory as FactoryContract } from '../generated/Maker/Factory'
-import { Swap as SwapEvent } from '../generated/Maker/Pair'
+import { LogConvert as LogConvertEvent } from '../generated/Maker/Maker'
+
 
 function getMaker(): Maker {
   const id = SUSHI_MAKER_ADDRESS.toHex()
@@ -33,47 +32,40 @@ function getServer(address: Address): Server {
   return server as Server
 }
 
-export function served(event: SwapEvent): void {
-  // check if it's a swap from SushiMaker to SushiBar
-  if (event.params.sender != SUSHI_MAKER_ADDRESS && event.params.to != SUSHI_BAR_ADDRESS) {
-    return
-  }
+export function handleLogConvert(event: LogConvertEvent): void {
+  
 
   const maker = getMaker()
-
-  const server = getServer(event.transaction.from)
-
-  const transaction = event.transaction
-
-  const input = transaction.input.toHexString()
-
-  const token0 = Address.fromString(ByteArray.fromHexString(input.substring(34, 74)).toHexString())
-  const token1 = Address.fromString(ByteArray.fromHexString(input.substring(98, 138)).toHexString())
+  const server = getServer(event.params.server)
 
   const factoryContract = FactoryContract.bind(FACTORY_ADDRESS)
-  const token0Contract = ERC20Contract.bind(token0)
-  const token1Contract = ERC20Contract.bind(token1)
 
-  const pair = factoryContract.getPair(token0, token1)
-
-  const sushiServed = event.params.amount0Out.divDecimal(BIG_DECIMAL_1E18)
+  const pair = factoryContract.getPair(event.params.token0, event.params.token1)
+ 
+  const amountSUSHI = event.params.amountSUSHI.toBigDecimal()
+  
+  // log.debug('server amount0 amount1 token0 token1 ; {} {} {} {} {}', [event.params.server.toHex(), event.params.amount0.toString(), event.params.amount1.toString(), event.params.token0.toHex(), event.params.token1.toHex()])
 
   const id = pair.toHex().concat('-').concat(event.block.number.toString())
   const serving = new Serving(id)
   serving.maker = maker.id
   serving.server = server.id
-  serving.tx = transaction.hash
-  serving.pair = pair
-  serving.token0 = token0
-  serving.token1 = token1
+  serving.tx = event.transaction.hash
+  serving.token0 = event.params.token0
+  serving.token1 = event.params.token1
+  serving.amount0 = event.params.amount0
+  serving.amount1 = event.params.amount1
   serving.block = event.block.number
   serving.timestamp = event.block.timestamp
-  serving.sushiServed = sushiServed
+  serving.amountSUSHI = event.params.amountSUSHI
   serving.save()
 
-  server.sushiServed = server.sushiServed.plus(sushiServed)
+  log.debug('sushiServed amount0 tx  ; {} {} {}', [serving.amountSUSHI.toString(), event.params.amount0.toString(), serving.tx.toHexString()])
+
+  server.sushiServed = server.sushiServed.plus(amountSUSHI)
   server.save()
 
-  maker.sushiServed = maker.sushiServed.plus(sushiServed)
+  maker.sushiServed = maker.sushiServed.plus(amountSUSHI)
   maker.save()
+
 }
