3daf033d343c74652cca37ecd59eaffc92e27f17
0xJiro <jirosushi@protonmail.com>
2021-07-20 01:23:50 -0700

    Updated minichef subgraph to make use of CloneRewarderTime rewarders

--- subgraphs/minichef/src/entities/rewarder.ts
+++ subgraphs/minichef/src/entities/rewarder.ts
@@ -1,27 +1,43 @@
-import { ADDRESS_ZERO, BIG_INT_ZERO, COMPLEX_REWARDER, NATIVE } from "const";
-import { Address, ethereum } from "@graphprotocol/graph-ts";
+import { ADDRESS_ZERO, BIG_INT_ZERO, COMPLEX_REWARDER, NATIVE } from "const"
+import { Address, ethereum } from "@graphprotocol/graph-ts"
 
-import { ComplexRewarderTime as ComplexRewarderTemplate } from "../../generated/templates";
-import { Rewarder } from "../../generated/schema";
+import { ComplexRewarderTime as ComplexRewarderTemplate } from "../../generated/templates"
+import { CloneRewarderTime as CloneRewarderTemplate } from "../../generated/templates"
+import { CloneRewarderTime as CloneRewarderContract } from "../../generated/templates/CloneRewarderTime/CloneRewarderTime"
+import { Rewarder } from "../../generated/schema"
 
 export function getRewarder(address: Address, block: ethereum.Block): Rewarder {
-  let rewarder = Rewarder.load(address.toHex());
+  let rewarder = Rewarder.load(address.toHex())
 
   if (rewarder === null) {
-    rewarder = new Rewarder(address.toHex());
-    rewarder.rewardToken = ADDRESS_ZERO;
-    rewarder.rewardPerSecond = BIG_INT_ZERO;
+    rewarder = new Rewarder(address.toHex())
+    rewarder.rewardToken = ADDRESS_ZERO
+    rewarder.rewardPerSecond = BIG_INT_ZERO
 
     if (address == COMPLEX_REWARDER) {
-      rewarder.rewardPerSecond = BIG_INT_ZERO;
-      rewarder.rewardToken = NATIVE;
-      ComplexRewarderTemplate.create(address);
+      rewarder.rewardPerSecond = BIG_INT_ZERO
+      rewarder.rewardToken = NATIVE
+      ComplexRewarderTemplate.create(address)
     }
+    else {
+      const rewarderContract = CloneRewarderContract.bind(address)
+      let rewardTokenResult = rewarderContract.try_rewardToken()
+      let rewardRateResult = rewarderContract.try_rewardPerSecond()
+
+      if (!rewardTokenResult.reverted) {
+        rewarder.rewardToken = rewardTokenResult.value
+      }
+      if (!rewardRateResult.reverted) {
+        rewarder.rewardPerSecond = rewardRateResult.value
+        CloneRewarderTemplate.create(address)
+      }
+    }
+
   }
 
-  rewarder.timestamp = block.timestamp;
-  rewarder.block = block.number;
-  rewarder.save();
+  rewarder.timestamp = block.timestamp
+  rewarder.block = block.number
+  rewarder.save()
 
-  return rewarder as Rewarder;
+  return rewarder as Rewarder
 }
---
+++ subgraphs/minichef/src/mappings/clone-rewarder.ts
@@ -0,0 +1,14 @@
+import { Address, BigInt, log } from '@graphprotocol/graph-ts'
+
+import { LogRewardPerSecond } from '../../generated/templates/CloneRewarderTime/CloneRewarderTime'
+import { getRewarder } from '../entities'
+
+export function logRewardPerSecond(event: LogRewardPerSecond): void {
+  log.info('[Minichef:CloneRewarder] Log Reward Per Second {}', [
+    event.params.rewardPerSecond.toString()
+  ])
+
+  const rewarder = getRewarder(event.address, event.block)
+  rewarder.rewardPerSecond = event.params.rewardPerSecond
+  rewarder.save()
+}
