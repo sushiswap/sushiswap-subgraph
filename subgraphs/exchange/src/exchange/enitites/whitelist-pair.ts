da0f6c2876a4a207a59fa8e3f5d4b86538d30c9b
Jonathan Morua <jona16@gmail.com>
2021-07-25 08:45:50 -0700

    remove log and update formatting

--- subgraphs/exchange/src/exchange/enitites/whitelist-pair.ts
+++ subgraphs/exchange/src/exchange/enitites/whitelist-pair.ts
@@ -1,15 +1,17 @@
-import { log } from '@graphprotocol/graph-ts'
-import { WhitelistPair } from '../../../generated/schema'
+import { WhitelistPair } from "../../../generated/schema";
 
-export function createOrLoadWhitelistPair(whiteListAddress: string, tokenId: string): WhitelistPair {
-  const whiteListPairId = whiteListAddress.concat('-').concat(tokenId)
+export function createOrLoadWhitelistPair(
+  whiteListAddress: string,
+  tokenId: string
+): WhitelistPair {
+  const whiteListPairId = whiteListAddress.concat("-").concat(tokenId);
 
-  let whiteListPair = WhitelistPair.load(whiteListPairId)
+  let whiteListPair = WhitelistPair.load(whiteListPairId);
 
   if (whiteListPair === null) {
-    whiteListPair = new WhitelistPair(whiteListPairId)
-    whiteListPair.save()
+    whiteListPair = new WhitelistPair(whiteListPairId);
+    whiteListPair.save();
   }
 
-  return whiteListPair as WhitelistPair
+  return whiteListPair as WhitelistPair;
 }
