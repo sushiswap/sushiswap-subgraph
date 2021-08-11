ea9ee4e56e481a47d3ae3c93631bc3fef6307ca4
0xJiro <jirosushi@protonmail.com>
2021-08-11 00:21:31 -0700

    Updated maker and added studio deploy entrypoints

---
+++ subgraphs/maker/src/entities/index.ts
@@ -0,0 +1,2 @@
+export * from './maker'
+export * from './server'
--- subgraphs/maker/src/entities/server.ts
+++ subgraphs/maker/src/entities/server.ts
@@ -1,17 +1,23 @@
-import { Address } from '@graphprotocol/graph-ts'
-import { BIG_DECIMAL_ZERO, SUSHI_MAKER_ADDRESS } from '../../../../packages/constants'
-import { Server } from '../generated/schema'
+import { Server } from '../../generated/schema'
+import { Address, ethereum } from '@graphprotocol/graph-ts'
+import { BIG_DECIMAL_ZERO, BIG_INT_ZERO } from 'const'
+import { getMaker } from './maker'
 
-export function getServer(address: Address): Server {
-  const id = address.toHex()
-  let server = Server.load(id)
+export function getServer(address: Address, block: ethereum.Block): Server {
+  const maker = getMaker(block)
+
+  let server = Server.load(address.toHex())
 
   if (server === null) {
-    server = new Server(id)
-    server.maker = SUSHI_MAKER_ADDRESS.toHex()
-    server.sushiServed = BIG_DECIMAL_ZERO
-    server.save()
+    server = new Server(address.toHex())
+    server.maker = maker.id
+    server.sushiServed = BIG_INT_ZERO
+    server.totalServings = BIG_DECIMAL_ZERO
   }
+
+  server.timestamp = block.timestamp
+  server.block = block.number
+  server.save()
 
   return server as Server
 }
